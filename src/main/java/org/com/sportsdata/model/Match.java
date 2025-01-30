42b0539192c388d1c5671269a70bce5971d4d635
yigir <yiit625@gmail.com>
2025-01-30 12:06:45 +0100

    Should Start Match and Match model has been implemented

--- src/main/java/org/com/sportsdata/model/Match.java
+++ src/main/java/org/com/sportsdata/model/Match.java
@@ -1,11 +1,6 @@
 package org.com.sportsdata.model;
 
 public class Match {
-    private final String homeTeam;
-    private final String awayTeam;
-
     public Match(String homeTeam, String awayTeam) {
-        this.homeTeam = homeTeam;
-        this.awayTeam = awayTeam;
     }
 }
