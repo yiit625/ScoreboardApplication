3f6db40a592f22d511081534f90f71a7626fa6f8
yigir <yiit625@gmail.com>
2025-01-31 15:48:40 +0100

    Catch error in order to avoid termination of the project -- Implementation.

--- src/main/java/org/com/sportsdata/Scoreboard.java
+++ src/main/java/org/com/sportsdata/Scoreboard.java
@@ -1,6 +1,9 @@
 package org.com.sportsdata;
 
 import org.com.sportsdata.comparator.MatchComparator;
+import org.com.sportsdata.exceptions.MatchNotFoundException;
+import org.com.sportsdata.exceptions.ScoreException;
+import org.com.sportsdata.exceptions.TeamNameException;
 import org.com.sportsdata.model.Match;
 import org.com.sportsdata.util.MatchFinder;
 import org.com.sportsdata.validators.MatchValidator;
@@ -20,21 +23,36 @@ public class Scoreboard {
     }
 
     public void startMatch(String homeTeam, String awayTeam) {
-        MatchValidator.validateTeams(new ArrayList<>(matches.values()), homeTeam, awayTeam);
-        matches.put(MatchFinder.getMatchKey(homeTeam, awayTeam), new Match(homeTeam, awayTeam, clock));
+        try {
+            MatchValidator.validateTeams(new ArrayList<>(matches.values()), homeTeam, awayTeam);
+            matches.put(MatchFinder.getMatchKey(homeTeam, awayTeam), new Match(homeTeam, awayTeam, clock));
+            System.out.println("Match started!");
+        } catch (TeamNameException e) {
+            System.err.println("Failed to start match: " + e.getMessage());
+        }
     }
 
     public void finishMatch(String homeTeam, String awayTeam) {
-        MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
-        matches.remove(MatchFinder.getMatchKey(homeTeam, awayTeam));
+        try {
+            MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
+            matches.remove(MatchFinder.getMatchKey(homeTeam, awayTeam));
+            System.out.println("Match finished!");
+        } catch (MatchNotFoundException e) {
+            System.err.println("Failed to finish match: " + e.getMessage());
+        }
     }
 
    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-       MatchValidator.validateScores(homeScore, awayScore);
-       MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
+       try {
+           MatchValidator.validateScores(homeScore, awayScore);
+           MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
 
-       String key = MatchFinder.getMatchKey(homeTeam, awayTeam);
-       matches.computeIfPresent(key, (k, match) -> match.updateScore(homeScore, awayScore));
+           String key = MatchFinder.getMatchKey(homeTeam, awayTeam);
+           matches.computeIfPresent(key, (k, match) -> match.updateScore(homeScore, awayScore));
+           System.out.println("Match updated!");
+       } catch (ScoreException | MatchNotFoundException e) {
+           System.err.println("Failed to update score: " + e.getMessage());
+       }
     }
 
     public List<String> getSummary() {
