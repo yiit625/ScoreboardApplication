626115f4f0b809f8789a30c2a8a9afa75aa3eb47
yigir <yiit625@gmail.com>
2025-01-31 15:48:23 +0100

    Catch error in order to avoid termination of the project.

--- src/test/java/com/sportsdata/ScoreboardTest.java
+++ src/test/java/com/sportsdata/ScoreboardTest.java
@@ -1,32 +1,40 @@
 package com.sportsdata;
 
 import org.com.sportsdata.Scoreboard;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
-import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ScoreboardTest {
     Clock fixedClock = Clock.fixed(Instant.parse("2024-01-30T12:00:00Z"), ZoneId.of("UTC"));
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private Scoreboard scoreboard;
+
+    @BeforeEach
+    void setUp() {
+        scoreboard = new Scoreboard(fixedClock);
+        System.setErr(new PrintStream(errContent));
+    }
 
     @Test
     void shouldStartNewMatch() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
+
         assertEquals(1, scoreboard.getSummary().size());
     }
 
     @Test
     void shouldUpdateMatch() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
         scoreboard.updateScore("Team A", "Team B", 2, 1);
 
@@ -35,15 +43,14 @@ public class ScoreboardTest {
 
     @Test
     void shouldFinishMatch() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
         scoreboard.finishMatch("Team A", "Team B");
+
         assertTrue(scoreboard.getSummary().isEmpty());
     }
 
     @Test
     void shouldReturnSummaryOrderByTotalScore() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
         scoreboard.startMatch("Team C", "Team D");
         scoreboard.updateScore("Team A", "Team B", 2, 2);
@@ -62,10 +69,10 @@ public class ScoreboardTest {
             "Team A, Team B, One of the teams is already in a match"
     })
     void shouldValidateTeamNames(String homeTeam, String awayTeam, String expectedMessage) {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> scoreboard.startMatch(homeTeam, awayTeam));
-        assertEquals(expectedMessage, exception.getMessage());
+        scoreboard.startMatch(homeTeam, awayTeam);
+
+        assertTrue(errContent.toString().contains(expectedMessage));
     }
 
     @ParameterizedTest
@@ -74,23 +81,21 @@ public class ScoreboardTest {
             "2, -1, Scores cannot be negative"
     })
     void shouldNotAllowNegativeScores(int homeScore, int awayScore, String expectedMessage) {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         scoreboard.startMatch("Team A", "Team B");
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                scoreboard.updateScore("Team A", "Team B", homeScore, awayScore));
-        assertEquals(expectedMessage, exception.getMessage());
+        scoreboard.updateScore("Team A", "Team B", homeScore, awayScore);
+
+        assertTrue(errContent.toString().contains(expectedMessage));
     }
 
     @Test
     void shouldNotUpdateScoreOrFinishNonExistentMatch() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
-        assertThrows(NoSuchElementException.class, () ->
-                scoreboard.updateScore("Team X", "Team Y", 1, 2));
+        scoreboard.updateScore("Team X", "Team Y", 1, 2);
+
+        assertTrue(errContent.toString().contains("Match cannot be found."));
     }
 
     @Test
     void shouldReturnEmptySummaryWhenNoMatch() {
-        Scoreboard scoreboard = new Scoreboard(fixedClock);
         assertTrue(scoreboard.getSummary().isEmpty());
     }
 }
