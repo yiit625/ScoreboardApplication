ce9ffb9eadf7c0bd7a0465458d220324af0e7b73
yigir <yiit625@gmail.com>
2025-02-01 17:18:35 +0100

    Java Docs added for both class. Replaced computeIfAbsent with compute since we already validate that match

--- src/main/java/org/com/sportsdata/model/Match.java
+++ src/main/java/org/com/sportsdata/model/Match.java
@@ -2,19 +2,47 @@ package org.com.sportsdata.model;
 
 import java.time.Clock;
 
+/**
+ * Represents a sports match between two teams.
+ * This class is immutable.
+ */
 public record Match (String homeTeam, String awayTeam, int homeScore, int awayScore, long startTime) {
+    /**
+     * Constructs a new Match with the specified teams and starting time.
+     *
+     * @param homeTeam The name of the home team.
+     * @param awayTeam The name of the away team.
+     * @param clock    The Clock instance to use for obtaining the current time.
+     */
     public Match(String homeTeam, String awayTeam, Clock clock) {
         this(homeTeam, awayTeam, 0, 0, clock.instant().toEpochMilli());
     }
 
+    /**
+     * Creates a new Match with updated scores.
+     *
+     * @param homeScore The updated score of the home team.
+     * @param awayScore The updated score of the away team.
+     * @return A new Match object with the updated scores.
+     */
     public Match updateScore (int homeScore, int awayScore) {
        return new Match(homeTeam, awayTeam, homeScore, awayScore, startTime);
     }
 
+    /**
+     * Calculates the total score of the match.
+     *
+     * @return The sum of homeScore and awayScore.
+     */
     public int getTotalScore() {
         return homeScore + awayScore;
     }
 
+    /**
+     * Returns a summary string representing the match.
+     *
+     * @return A string in the format "homeTeam homeScore-awayScore awayTeam".
+     */
     public String getSummary() {
         return homeTeam + " " + homeScore + "-" + awayScore + " " + awayTeam;
     }
--- src/main/java/org/com/sportsdata/service/Scoreboard.java
+++ src/main/java/org/com/sportsdata/service/Scoreboard.java
@@ -9,22 +9,40 @@ import org.com.sportsdata.util.MatchFinder;
 import org.com.sportsdata.validators.MatchValidator;
 
 import java.time.Clock;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This class manages the scoreboard for sports matches.
+ * It provides methods to start, finish, and update scores for matches.
+ * It also provides a summary of the current matches.
+ */
 public class Scoreboard {
-    private final Map<String, Match> matches = new HashMap<>();
+    private final Map<String, Match> matches = new ConcurrentHashMap<>();
     private final Clock clock;
 
+    /**
+     * Constructs a new Scoreboard instance with the given Clock.
+     *
+     * @param clock The Clock instance to use for obtaining the current time.
+     */
     public Scoreboard(Clock clock) {
         this.clock = clock;
     }
 
+    /**
+     * Starts a new match between the specified home and away teams.
+     *
+     * @param homeTeam The name of the home team.
+     * @param awayTeam The name of the away team.
+     * @throws TeamNameException If the team names are invalid or a match between these teams is already in progress.
+     */
     public void startMatch(String homeTeam, String awayTeam) {
         try {
             MatchValidator.validateTeams(new ArrayList<>(matches.values()), homeTeam, awayTeam);
+
             matches.put(MatchFinder.getMatchKey(homeTeam, awayTeam), new Match(homeTeam, awayTeam, clock));
             System.out.println("Match started!");
         } catch (TeamNameException e) {
@@ -32,9 +50,17 @@ public class Scoreboard {
         }
     }
 
+    /**
+     * Finishes an existing match between the specified teams.
+     *
+     * @param homeTeam The name of the home team.
+     * @param awayTeam The name of the away team.
+     * @throws MatchNotFoundException If no match is found for the given teams.
+     */
     public void finishMatch(String homeTeam, String awayTeam) {
         try {
             MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
+
             matches.remove(MatchFinder.getMatchKey(homeTeam, awayTeam));
             System.out.println("Match finished!");
         } catch (MatchNotFoundException e) {
@@ -42,25 +68,38 @@ public class Scoreboard {
         }
     }
 
-   public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-       try {
-           MatchValidator.validateScores(homeScore, awayScore);
-           MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
+    /**
+     * Updates the score for an existing match.
+     *
+     * @param homeTeam The name of the home team.
+     * @param awayTeam The name of the away team.
+     * @param homeScore The current score of the home team.
+     * @param awayScore The current score of the away team.
+     * @throws ScoreException If the scores are invalid.
+     * @throws MatchNotFoundException If no match is found for the given teams.
+     */
+    public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
+        try {
+            MatchValidator.validateScores(homeScore, awayScore);
+            MatchValidator.validateMatchExists(matches, homeTeam, awayTeam);
 
-           String key = MatchFinder.getMatchKey(homeTeam, awayTeam);
-           matches.computeIfPresent(key, (k, match) -> match.updateScore(homeScore, awayScore));
-           System.out.println("Match updated!");
-       } catch (ScoreException | MatchNotFoundException e) {
-           System.err.println("Failed to update score: " + e.getMessage());
-       }
+            matches.compute(MatchFinder.getMatchKey(homeTeam, awayTeam), (k, match) ->
+                    match != null ? match.updateScore(homeScore, awayScore) : null);
+            System.out.println("Match updated!");
+        } catch (ScoreException | MatchNotFoundException e) {
+            System.err.println("Failed to update score: " + e.getMessage());
+        }
     }
 
+    /**
+     * Retrieves a summary of all active matches.
+     *
+     * @return A list of strings, each representing the summary of an active match.
+     */
     public List<String> getSummary() {
-        return List.copyOf(
-                matches.values().stream()
-                        .sorted(new MatchComparator())
-                        .map(Match::getSummary)
-                        .toList()
-        );
+        return matches.values().stream()
+                .sorted(new MatchComparator())
+                .map(Match::getSummary)
+                .toList();
     }
 }
